b4786b4cf9c2b2eea21c0ae183ad76bb45e6ac26
xuan1202 <1912425817@qq.com>
2022-06-01 15:51:48 +0800

    fix: Hello组件添加span哈哈哈

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -8,6 +8,7 @@ interface Greeting {
 const Hello: React.FC<Greeting> = (props: Greeting) => {
     return (
         <div>
+            <span>哈哈哈</span>
             <h3>Hello组件</h3>
             <p>{props.name === 'tom' ? props.name + 'hanks' : props.name}</p>
         </div>
