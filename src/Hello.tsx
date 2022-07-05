0929e27a5c7b9dfce3efb613a1233e54057cd1a8
peixuan.gu.ext <peixuan.gu.ext@momenta.ai>
2022-07-05 23:13:26 +0800

    chore: 添加.eslinttrc.json。

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,21 +1,19 @@
 /** @format */
 
-import React from 'react'
+import React from 'react';
 
 interface Greeting {
     name: string
 }
 const Hello: React.FC<Greeting> = (props: Greeting) => {
+    const a='zzzz';
     return (
         <div>
             <h3>Hello组件</h3>
-
-
-
             <span>哈哈哈</span>
             <span>111111</span>
             <p>{props.name === 'tom' ? props.name + 'hanks' : props.name}</p>
         </div>
-    )
-}
-export default Hello
+    );
+};
+export default Hello;
---
+++ src/test.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from 'react';
+
+interface Greeting {
+    name: string
+}
+const Test: React.FC<Greeting> = (props: Greeting) => {
+    const a='wwwwwwww';
+    return (
+        <div>
+        </div>
+    );
+};
+export default Test;
